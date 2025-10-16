1a0945aa665cfec7e924ad3ea657cacca9f06371
Felipe Akahoshi <felipeakahoshide@gmail.com>
2025-10-16 13:33:22 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "ibm_resource_group" "name" {
+  name = var.skibidigroup
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "regiao"
+  type = string
+}
+
+variable "zone" {
+  description = "zona"
+  type = string
+}
+
+variable "ibmcloud_api_key_p1nonpci" {
+  description = "api"
+  type = string
+}
+
+variable "skibidigroup" {
+  description = "nome do resource group"
+  type = string
+}
